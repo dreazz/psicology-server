00965481972aade74c2b3ee9d2720daea441518b
asilveira <asilveira@niikiis.com>
2020-05-02 23:33:34 +0200

    fix cors

--- index.js
+++ index.js
@@ -5,15 +5,11 @@ const app = express();
 const cors = require('cors')
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  // res.header("Access-Control-Allow-Origin", "http://localhost:8000");
-  // res.header("Access-Control-Allow-Origin", "https://evaminerva.com");
-  // res.header("Access-Control-Allow-Origin", "https://evaminerva.com");  
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-  //rest of your app.get/post/whatevers
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8000/');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
 })
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
