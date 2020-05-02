c7b2a6461265aadb0dee727776647258803530cd
asilveira <asilveira@niikiis.com>
2020-05-02 23:41:22 +0200

    fix cors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const cors = require('cors')
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
@@ -14,22 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 require('dotenv').config({ path: '.env' });
-// app.use(cors({
-//   credentials: true,
-//   origin: "http://localhost:8000"
-// }));
-// var whitelist = ['http://localhost:8000/', 'https://evaminerva.com/', 'https://pisco-javi.netlify.app/']
-// var corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// }
-// app.options('*', cors())
-// app.use(cors(corsOptions))
+
 //Connect mongoose
 mongoose
   .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -46,7 +31,6 @@ const Form = require("./routes/Form");
 //Routes configuration
 app.use("/form", Form);
 
-console.log("HIII")
 
 app.listen(3000, function () {
   console.log("Example app listening on port 3000!");
