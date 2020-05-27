9cc335fd561ab0fbb0da6b35dd18d2db3edde53d
dreazz <agustinsilveira.96@gmail.com>
2020-05-27 17:49:58 +0200

    add access control allow origin

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.use(cors({
   origin: process.env.CORS,
   credentials: true
 }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin",process.env.CORS ); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
