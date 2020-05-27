653d9139ad1a4233d9dfad54786c11c0b15c51f6
dreazz <agustinsilveira.96@gmail.com>
2020-05-27 17:57:29 +0200

    add mor headers

--- index.js
+++ index.js
@@ -4,15 +4,21 @@ const bodyParser = require("body-parser")
 const app = express();
 const cors = require('cors')
 
-app.options('*', cors())
-app.use(cors({
-  origin: process.env.CORS,
-  credentials: true
-}));
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin",process.env.CORS ); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+ // Request methods you wish to allow
+ res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+ // Request headers you wish to allow
+ res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+ // Set to true if you need the website to include cookies in the requests sent
+ // to the API (e.g. in case you use sessions)
+ res.setHeader('Access-Control-Allow-Credentials', true);
+
+ // Pass to next layer of middleware
+ next();
 });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
